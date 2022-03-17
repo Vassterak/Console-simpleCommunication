2a62ab156043098136d57d7e0ad2660192ff793e
Vassterak <35633483+Vassterak@users.noreply.github.com>
2022-03-17 19:04:30 +0100

    Preparation for proper data ingestion

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -21,29 +21,16 @@ int main(void)
 	return 0;
 } */
 
-#define DATA_TRANSFER_PIN 2
-#define DATA_FISTBIT_DELAY 200
+#define DATA_TRANSFER_PIN 2 //pin number in register D
+#define DATA_FISTBIT_DELAY 200 //us (microseconds)
 
-uint8_t firstBit = 0;
+//for first bit indicating incomming packet (define tolerances for signal duration)
+#define START_TOLERANCE 470 //us
+#define END_TOLERANCE 530 //us
 
-void readBits()
+void readIncommingPacket()
 {
-	Serial.println("data");
-	firstBit = 0;
-}
 
-void waitingForFirstBit()
-{
-	int counter1;
-
-	while (1)
-	{
-		if (readValueAVR(DATA_TRANSFER_PIN) == 1)
-		{
-			counter1++;
-			_delay_us(10);
-		}
-	}
 }
 
 int main (void)
@@ -60,23 +47,24 @@ int main (void)
 
 	while (1)
 	{
+		//Check if value on port is high
    		if (readValueAVR(DATA_TRANSFER_PIN) == 1)
 		{
-			//get time of first signal rise (microseconds)
+			//get time of first signal rise (get cpu time in microseconds)
  			timeShift = micros();
 
-			//Do nothing while incomming value is high
+			//Do nothing while port value is high
 			while (readValueAVR(DATA_TRANSFER_PIN) == 1)
 				;
 			
-			//get time of first signal fall (microseconds)
+			//get time of signal fall change 1 -> 0 (get cpu time in microseconds)
 			timeShift2 = micros();
 
 			//Check if data transmission started (each packet has starting value, used for sync)
 			if (timeShift2 - timeShift <= 530 && timeShift2 - timeShift > 470)
 			{
-				Serial.println("start");
 				_delay_us(2000);
+				readIncommingPacket();
 			}
 			else
 			{
