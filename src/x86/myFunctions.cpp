572dc50b1fdc5cd0d53144090d2723b0732389bd
Vassterak <35633483+Vassterak@users.noreply.github.com>
2022-03-23 13:44:16 +0100

    Preparation for x86 part

---
+++ src/x86/myFunctions.cpp
@@ -0,0 +1,93 @@
+#include "myFunctions.h"
+
+int portState = 0, portAddress = 0;
+
+//---------------------------------------------Setup for school's x86 platform---------------------------------------------
+/* int setup(int newPortAddress)
+{
+	if (ioperm(newPortAddress,1,1) !=0)
+	{
+		printf("Program nemam pristup k portu.\n");
+		return 1;
+	}
+	
+	else
+	{
+		portAddress = newPortAddress;
+		portState = 0;
+		outb(portState, newPortAddress);
+		printf("Program inicializoval hodnoty na 0.\n");
+		return 0;
+	}
+}; */
+
+//writeValue function for x86 platform
+/* int writeValue(int bitPos, int value)
+{
+	if (value == 1)
+		portState |= (1<<bitPos);
+	else
+		portState &= ~(1<<bitPos);
+	
+	printf("Write to port\n");
+	outb(portState, portAddress);
+	return portState;
+};
+
+int readReceivedValue()
+{
+	if (inb(PORT_ADDRESS)&(1<<0))
+		return 1;
+	else
+		return 0;
+}; */
+
+/*void myDelay(int delayMS)
+{
+	clock_t startTime = clock();
+	while (clock() < startTime + delayMS * 1000)
+	;
+};*/
+
+/*
+int myDelay2(long miliseconds)
+{
+   struct timespec rem;
+   struct timespec req= {
+       (int)(miliseconds / 1000),
+       (miliseconds % 1000) * 1000000
+   };
+
+   return nanosleep(&req , &rem);
+}*/
+
+//---------------------------------------------Setup for AVR platform---------------------------------------------
+void setup(int pinSelection, bool isTransmiter)
+{
+	//set register as output, leave other alone (for protection input is preferred)
+	if (isTransmiter)
+	{
+		DDRD = (0x01 << pinSelection);
+		PORTD = 0x00;
+	}
+		
+	else
+		DDRD = 0x00;
+}
+
+void writeValueAVR(int bitPos, int value)
+{
+	if (value == 1)
+		PORTD |= (1 << bitPos);
+	else
+		PORTD &= ~(1 << bitPos);
+}
+
+int readValueAVR(int bitPos)
+{
+ 	if ((PIND & (1 << bitPos)) == 0)
+		return 0;
+	else
+		return 1;
+	//return (PIND & (1 << bitPos));
+}
---
+++ src/x86/transmiter.cpp
@@ -0,0 +1,155 @@
+//#include <sys/io.h>
+//#include <stdio.h>
+//#include <time.h>
+#include "myFunctions.h"
+
+/* #define PORT_ADDRESS 0x300
+#define MAX_PACKET_SIZE 16 //in bytes, min size is 
+
+char userInput[16];
+int currentCharPosition = 0;
+
+void userInput()
+{
+    fgets(userInput,16,stdin);
+};
+
+int returnBinaryValue()
+{
+
+
+};
+
+int main(void)
+{	
+	//End program when port access is denied.
+	if (setup(PORT_ADDRESS) == 1)
+		return 1;
+	
+	writeValue(0,0); //set first bit to LOW (Transmit mode)
+	
+	while (1)
+	{
+		printf("Transmiter\n");
+		writeValue(1, 0);
+		delay(50);
+		writeValue(1, 1);
+		delay(50);
+	}
+	return 0;
+} */
+
+#define DATA_TRANSFER_PIN 2
+#define DATA_HOLD_TIME 998 //2us removed because that's the average time that is used for rest of the program
+
+//I aware that this step is not memory efficient whole int array takes up 512 bytes (when int is one byte) And I know that I can store just ascii values and convert it to binary on the fly before sending.
+//But for this purpose of school homework I have settled for this solution. So convert all ascii values to binary and save it in 2d array. (I know that I'm wasting a lot of memory)
+static uint8_t rawBinaryMessage[64][8], currentIndex = 0, sizeOfMessage = 0, checkSumNum[64];
+
+void ConvertToBinary(uint8_t asciiChar)
+{
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		if ((asciiChar & 1) == 1)
+		{
+			rawBinaryMessage[currentIndex][i] = 1;
+			checkSumNum[currentIndex]++;
+		}
+
+		else
+			rawBinaryMessage[currentIndex][i] = 0;
+
+		asciiChar = asciiChar >> 1;
+	}
+}
+
+void breakUpText(char text[])
+{
+	sizeOfMessage = strlen(text);
+
+	for (uint8_t i = 0; text[i] != '\0'; i++)
+	{
+		currentIndex = i;
+		ConvertToBinary(text[i]);
+	}
+}
+
+void startDelay()
+{
+	writeValueAVR(DATA_TRANSFER_PIN, 1);
+	_delay_us(500);
+	writeValueAVR(DATA_TRANSFER_PIN, 0);
+	_delay_us(2000);
+}
+
+//Sent 4bit checksum
+void checkSum(uint8_t packetID)
+{
+	uint8_t number = checkSumNum[packetID];
+	//Serial.println(number);
+
+	for (uint8_t i = 0; i < 4; i++)
+	{
+		if ((number >> (i)) & 1)
+		{
+			writeValueAVR(DATA_TRANSFER_PIN, 0);
+			_delay_us(DATA_HOLD_TIME);
+			writeValueAVR(DATA_TRANSFER_PIN, 1);
+			_delay_us(DATA_HOLD_TIME);
+		}
+		else
+		{
+			writeValueAVR(DATA_TRANSFER_PIN, 1);
+			_delay_us(DATA_HOLD_TIME);
+			writeValueAVR(DATA_TRANSFER_PIN, 0);
+			_delay_us(DATA_HOLD_TIME);
+		}
+	}
+	checkSumNum[packetID] = 0;
+}
+
+void dataSend()
+{
+	for (uint8_t i = 0; i < sizeOfMessage; i++)
+	{
+		//NEW PACKET
+		startDelay();
+		for (uint8_t j = 0; j < 8; j++)
+		{
+			if (rawBinaryMessage[i][j] == 0)
+			{
+				writeValueAVR(DATA_TRANSFER_PIN, 1);
+				_delay_us(DATA_HOLD_TIME);
+				writeValueAVR(DATA_TRANSFER_PIN, 0);
+				_delay_us(DATA_HOLD_TIME);
+			}
+			else
+			{
+				writeValueAVR(DATA_TRANSFER_PIN, 0);
+				_delay_us(DATA_HOLD_TIME);
+				writeValueAVR(DATA_TRANSFER_PIN, 1);
+				_delay_us(DATA_HOLD_TIME);
+			}
+		}
+		//_delay_us(2000);
+		checkSum(i);
+		writeValueAVR(DATA_TRANSFER_PIN, 0);
+		_delay_ms(20); //delay between packets
+	}
+}
+
+int main (void)
+{
+	setup(DATA_TRANSFER_PIN, 1);
+	init();
+	Serial.begin(9600);
+
+	while (1)
+	{
+		writeValueAVR(DATA_TRANSFER_PIN, 0);
+		breakUpText("helo");
+		dataSend();
+		_delay_ms(2000);;
+	}
+	return 0;
+}
