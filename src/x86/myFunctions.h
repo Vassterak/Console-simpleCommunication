a1b5ec218d297a89c9f694e034d03063ffeb1337
Vassterak <vaclavhorcicka@outlook.cz>
2022-03-27 22:53:06 +0200

    platform separation

---
+++ src/x86/myFunctions.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+
+#ifndef MYFUNCTIONS_H
+#define MYFUNCTIONS_H
+
+//---------------------------------------------Setup for school's x86 platform---------------------------------------------
+/* extern int portAddress;
+extern int portState;
+
+int setup(int newPortAddress);
+int writeValue(int bitPos, int value);
+int readReceivedValue();
+void delay(int delayMS); */
+
+//---------------------------------------------Setup for AVR platform---------------------------------------------
+void setup(int pinSelection, bool isTransmiter);
+void writeValueAVR(int bitPos, int value);
+int readValueAVR(int bitPos);
+//void myDelay(int delayMS);
+//void myDelay2(int delayMS);
+
+#endif
